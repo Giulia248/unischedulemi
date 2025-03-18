52ebe08f2569dba82b90f094c10eb98d83915c81
Giulia Floris <giulia.floris@accenture.com>
2025-03-18 18:11:03 +0100

    [ADD] login

---
+++ configs/GlobalConfigs.js
@@ -0,0 +1,16 @@
+
+const localUrl = "http://127.0.0.1:5500/";
+const remoteUrl = "";
+
+
+function commonLog(log, urgent) { // log: string - urgent: bool
+    const icon = urgent ? "❗️" : "";
+    console.log("[LOG]", icon, log);
+}
+
+// COLORS
+const uniDarkGreen = "#006d77";
+const uniLightGreen = "#83c5be";
+edf6f9
+ffddd2
+e29578
